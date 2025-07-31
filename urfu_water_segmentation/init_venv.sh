dd1e6844b30be266e90c73bde58051c83e132e0d
студент0171 <s0171@umtnew.imm.uran.ru>
2025-07-31 10:36:34 +0500

    fix

--- urfu_water_segmentation/init_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3.9 -m venv .venv
-source .venv/bin/activate
-
-pip install -U pip
-pip install poetry==1.8.3
-
-poetry install
--- urfu_water_segmentation/test.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-
-
-path = 'visualized_18450163/pred/00000000_pred.png'
-
-im_frame = Image.open(path)
-np_frame = np.array(im_frame.getdata())
-print(np_frame.max(), np_frame.min(), set(np_frame.tolist()), np_frame.shape)
